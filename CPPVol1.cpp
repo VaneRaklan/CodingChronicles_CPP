1e0ff2c69c273e98277fb821b3a094a32c82aa48
VaneRaklan <anton.manuel.ramirez@gmail.com>
2017-10-03 19:43:14 -0700

    Added comments and intro to data types

--- CPPVol1.cpp
+++ CPPVol1.cpp
@@ -6,3 +6,19 @@ int main(){
 	cout << "Hello World " << endl;
 	return 0;
 }
+
+
+void dataTypes(){
+	int	number_a = 0; 		//int stands for 'integer' which is a whole number value (either positive or negative)
+	double	number_b = 3.14; 	//double stands for 'double floating point' which is for longer numbers as well as decimals
+	bool	state_a  = false;	//bool stands for 'boolean' which only has two values - 'true' or 'false'
+	string  name	 = "Antonio";	//string doesn't stand for anything, but think of it as a string of characters
+}
+
+void comments(){
+/*
+This is a multiline comment
+*/
+
+//This is a single line comment
+}
